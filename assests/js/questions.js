3d65e7adfaf59ba43c4d1c2024b7a3fb2133bcc4
Jose Seto <joseseto8@gmail.com>
2023-04-13 11:45:09 -0700

    Added questions to questions script to be called in quizzes.

---
+++ assests/js/questions.js
@@ -0,0 +1,47 @@
+// creating an array and passing the number, questions, options, and answers
+let questions = [
+    {
+    numb: 1,
+    question: "Commonly used data types DO NOT include:",
+    answer: "Alerts",
+    options: [
+      "Alerts",
+      "Strings",
+      "Booleans",
+      "Numbers"
+    ]
+  },
+  
+    {
+    numb: 2,
+    question: "The condition in an if / else statement is enclosed within ____.",
+    answer: "parentheses",
+    options: ['quotes', 'curly brackets', 'parentheses', 'square brackets']
+  },
+
+    {
+    numb: 3,
+    question: "Arrays in JavaScript can be used to store ____.",
+    answer: "all of the above",
+    options: [
+      'numbers and strings',
+      'other arrays',
+      'booleans',
+      'all of the above',
+    ]
+  },
+
+    {
+    numb: 4,
+    question: "String values must be enclosed within ____ when being assigned to variables.",
+    answer: "quotes",
+    options: ['commas', 'curly brackets', 'quotes', 'parentheses']
+  },
+
+    {
+    numb: 5,
+    question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+    answer: "console.log",
+    options: ['JavaScript', 'terminal / bash', 'for loops', 'console.log']
+  },
+];
